d268da1332ce6f0ccb89ea279dbec45922a9395f
zmapleshine <715017@163.com>
2021-07-17 14:06:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Midnight Opera
+
+[![Build Status](https://travis-ci.org/zmapleshine/Midnight-Opera.svg?branch=main)](https://travis-ci.org/zmapleshine/Midnight-Opera)
+![Language](https://img.shields.io/badge/language-Java-orange.svg)
+![Language](https://img.shields.io/hexpm/l/plug)
+
+Use maven to build opentelemetry-java-instrumentation.
---
+++ bootstrap/src/main/java/com/minardream/midnightopera/AgentBootstrap.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+package com.minardream.midnightopera;
+
+import io.opentelemetry.javaagent.OpenTelemetryAgent;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * Premain-Class for the Midnight Opera Java agent.
+ *
+ * @author zmapleshine
+ * @date 2021/7/17 23:50
+ */
+public class AgentBootstrap {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println();
+        System.out.println(":: Midnight Opera ver" + AgentBootstrap.class.getPackage().getImplementationVersion() + " ::");
+        System.out.println(":: Website : https://github.com/zmapleshine/Midnight-Opera");
+        System.out.println(":: OpenTelemetry Java Instrumentation (v1.3.1-alpha) : https://github.com/open-telemetry/opentelemetry-java-instrumentation");
+        System.out.println();
+        OpenTelemetryAgent.premain(agentArgs, inst);
+    }
+
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+        OpenTelemetryAgent.agentmain(agentArgs, inst);
+    }
+}
