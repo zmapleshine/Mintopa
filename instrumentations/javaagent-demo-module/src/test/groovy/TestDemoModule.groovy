7ca9cd123d657c03026fe440cc82ecf51c88c626
zmapleshine <715017@163.com>
2021-08-01 23:40:48 +0800

    Add test demo

---
+++ instrumentations/javaagent-demo-module/src/test/groovy/TestDemoModule.groovy
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+import io.opentelemetry.instrumentation.test.AgentInstrumentationSpecification
+
+/**
+ * @author zmapleshine* @date 2021/7/31 16:43
+ */
+class TestDemoModule extends AgentInstrumentationSpecification {
+}
